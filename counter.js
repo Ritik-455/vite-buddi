d7b0b2075f984037d0523ca6d96a650d68fbab5e
Ritik <rvjaat25@gmail.com>
2023-12-26 12:09:21 +0530

    first-commit

---
+++ counter.js
@@ -0,0 +1,15 @@
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
+
+function openNav() {
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
